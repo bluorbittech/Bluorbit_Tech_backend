c52e9afa7bf2595a9564ad371b301a0de8a4b1f3
bluorbittech <help@bluorbittech.com>
2026-03-27 17:06:56 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDj2xGIKr4IoFrVr0dEOihlRgKvOlCtpVw",
-  authDomain: "bluorbittech.firebaseapp.com",
-  projectId: "bluorbittech",
-  storageBucket: "bluorbittech.firebasestorage.app",
-  messagingSenderId: "311554595735",
-  appId: "1:311554595735:web:a477a3d8af2ea64de102d2",
-  measurementId: "G-QHV6KR99JN",
+  apiKey: "apikey",
+  authDomain: "doamin",
+  projectId: "id",
+  storageBucket: "bucket",
+  messagingSenderId: "id",
+  appId: "appid",
+  measurementId: "mid",
 };
 
 const app = initializeApp(firebaseConfig);
